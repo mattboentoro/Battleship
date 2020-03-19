ad08e82e910d6af15b6dd2bf94b8ea54d11547bf
Matthew Boentoro <mattboentoro@yahoo.com>
2020-03-19 12:53:43 -0700

    Add files via upload

---
+++ cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/AiPlayer.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/AiPlayer.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Attack.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Attack.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/AttackResult.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/AttackResult.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Board.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Board.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Cell.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Cell.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/CheatingAI.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/CheatingAI.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Controller.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Controller.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/GameAttributes.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/GameAttributes.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/HumanPlayer.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/HumanPlayer.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/HuntDestroyAI.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/HuntDestroyAI.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Model.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Model.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Move.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Move.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Player.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Player.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/PlayerConfiguration.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/PlayerConfiguration.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/RandomAI.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/RandomAI.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/ShipPlacement.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/ShipPlacement.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/StandardView.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/StandardView.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/Utility.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/Utility.cpp.o"
+  "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/MVC/View.cpp" "/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/cmake-build-debug/MVC/CMakeFiles/BattleshipMVC.dir/View.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEFAULT_CONFIGURATION_LOCATION=\"/cygdrive/c/Users/Matt/Documents/CLionProjects/BattleShip/GameConfigurations/DefaultConfiguration.txt\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MVC/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
